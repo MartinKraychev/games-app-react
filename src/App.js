0e04001395ed5696591aedfa683defb68e946b2b
MartinKraychev <83907783+MartinKraychev@users.noreply.github.com>
2023-10-08 18:14:49 +0100

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { Routes, Route } from 'react-router-dom'
+
 import { Login } from "./auth-components/login";
 import { Register } from "./auth-components/register";
 import { Header } from "./base-components/header";
@@ -6,19 +8,23 @@ import { Catalog } from "./game-components/catalog";
 import { Create } from "./game-components/create";
 import { Details } from "./game-components/details";
 import { Edit } from "./game-components/edit";
+import { Logout } from './auth-components/logout';
 
 function App() {
     return (
         <div id="box">
             <Header />
             <main id="main-content">
-                <Home />
-                <Login />
-                <Register />
-                <Create />
-                <Edit />
-                <Details />
-                <Catalog />
+                <Routes>
+                    <Route path="/" element={<Home />}/>
+                    <Route path="/login" element={<Login />}/>
+                    <Route path="/register" element={<Register />}/>
+                    <Route path="/logout" element={<Logout />}/>
+                    <Route path="/games" element={<Catalog />}/>
+                    <Route path="/games/create" element={<Create />}/>
+                    <Route path="/games/edit" element={<Edit />}/>
+                    <Route path="/games/:gameId" element={<Details />}/>
+                </Routes>
             </main>
         </div>
     );
